7a68f07f6febf1caee3371fed8611eb43b8919dc
junsukLim <junsuk9874@naver.com>
2021-09-05 16:42:58 +0900

    2nd commit

--- src/com/example/lab1/Maiun.java
+++ src/com/example/lab1/Maiun.java
@@ -7,9 +7,9 @@ public class Maiun {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		System.out.println("Hello World!!!\n");
-		System.out.print("이름 입력 : ");
-		String name = s.next();
-		System.out.println("["+name+"]님 환영합니다~");
+		System.out.print("학번 입력 : ");
+		int student_number = s.nextInt();
+		System.out.println("["+student_number+"]님 환영합니다~");
 	}
 
 }
